bb0069e5a6ff48205eb2a517347a925802f4ce58
Raghavkhare12 <raghav122006@gmail.com>
2026-02-23 01:10:31 +0530

    DAY-9

---
+++ Union_of_arrays.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        vector<int> uni;
+        set<int> st;
+        for(int x : a){
+            st.insert(x);
+        }
+        for(int y : b){
+            st.insert(y);
+        }
+        for(int z : st){
+            uni.push_back(z);
+        }
+        return uni;
+    }
+};
+int main(){
+    vector<int> a= {1, 4, 2, 1, 3, 6, 4, 82};
+    vector<int> b= {5, 49, 2, 2, 3, 69, 44, 73, 83};
+    Solution obj;
+    vector<int> result =obj.findUnion(a, b);
+    for(int x : result){
+        cout << x << " ";
+    }
+    return 0;
+}
+/*Union of Arrays with Duplicates
+Difficulty: EasyAccuracy: 42.22%Submissions: 484K+Points: 2Average Time: 10m
+You are given two arrays a[] and b[], return the Union of both the arrays in any order.
+
+The Union of two arrays is a collection of all distinct elements present in either of the arrays. If an element appears more than once in one or both arrays, it should be included only once in the result.
+
+Note: Elements of a[] and b[] are not necessarily distinct.
+Note that, You can return the Union in any order but the driver code will print the result in sorted order only.
+
+Examples:
+
+Input: a[] = [1, 2, 3, 2, 1], b[] = [3, 2, 2, 3, 3, 2]
+Output: [1, 2, 3]
+Explanation: Union set of both the arrays will be 1, 2 and 3.
+Input: a[] = [1, 2, 3], b[] = [4, 5, 6] 
+Output: [1, 2, 3, 4, 5, 6]
+Explanation: Union set of both the arrays will be 1, 2, 3, 4, 5 and 6.
+Input: a[] = [1, 2, 1, 1, 2], b[] = [2, 2, 1, 2, 1] 
+Output: [1, 2]
+Explanation: Union set of both the arrays will be 1 and 2.
+Constraints:
+1 ≤ a.size(), b.size() ≤ 106
+0 ≤ a[i], b[i] ≤ 105
+*/
