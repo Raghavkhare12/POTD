650b0fa1637d08336c13347854d5bb9c20bef4b2
Raghavkhare12 <raghav122006@gmail.com>
2026-03-02 21:37:12 +0530

    DAY-16

---
+++ Trapping_rain_water.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxWater(vector<int> &arr) {
+        int left = 0, right = arr.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left <= right) {
+            if (arr[left] <= arr[right]) {
+                if (arr[left] >= leftMax)
+                    leftMax = arr[left];
+                else
+                    water += leftMax - arr[left];
+                left++;
+            } 
+            else {
+                if (arr[right] >= rightMax)
+                    rightMax = arr[right];
+                else
+                    water += rightMax - arr[right];
+                right--;
+            }
+        }
+
+        return water;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    Solution obj;
+    cout << obj.maxWater(arr);
+
+    return 0;
+}
+/*Trapping Rain Water
+Difficulty: HardAccuracy: 33.14%Submissions: 505K+Points: 8Average Time: 20m
+Given an array arr[] with non-negative integers representing the height of blocks. If the width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+
+Examples:
+
+Input: arr[] = [3, 0, 1, 0, 4, 0 2]
+Output: 10
+Explanation: Total water trapped = 0 + 3 + 2 + 3 + 0 + 2 + 0 = 10 units.
+
+Input: arr[] = [3, 0, 2, 0, 4]
+Output: 7
+Explanation: Total water trapped = 0 + 3 + 1 + 3 + 0 = 7 units.
+Input: arr[] = [1, 2, 3, 4]
+Output: 0
+Explanation: We cannot trap water as there is no height bound on both sides.
+Input: arr[] = [2, 1, 5, 3, 1, 0, 4]
+Output: 9
+Explanation: Total water trapped = 0 + 1 + 0 + 1 + 3 + 4 + 0 = 9 units.
+Constraints:
+1 < arr.size() < 105
+0 < arr[i] < 103
+*/
